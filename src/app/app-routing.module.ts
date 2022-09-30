15c9224c321675000c2a0c84b969074395ee0ef1
JohannChoo <62107810+DuoX371@users.noreply.github.com>
2022-10-01 01:32:32 +0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './content/home/home.component';
 import { LoginComponent } from './content/login/login.component';
+import { MainComponent } from './main/main.component';
 import { TestComponent } from './content/test/test.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, data: {title: 'Home'}},
-  {path: 'test', component: TestComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: '', component: MainComponent, children: [
+    {path: '', component: HomeComponent, data: {title: 'Home'}},
+    {path: 'test', component: TestComponent, data:{title: 'Data'}},
+  ]}
 ];
 
 @NgModule({
