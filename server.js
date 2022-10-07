dae651cbc230da3fe958390ecfeec4fc32d3aec0
DuoX371 <62107810+DuoX371@users.noreply.github.com>
2022-10-07 08:52:43 +0800

    test

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+//use express to create a server
+const app = require('./backend/app');
+const port = 3000;
+
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
